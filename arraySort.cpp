3084d19a7be3ed61d308e1b17285b983e6e37849
Jonny Smyth <jmyth742@gmail.com>
2021-04-29 21:42:08 +0200

    array sort done, using binary sort, still to do add tests and cleanup

---
+++ arraySort.cpp
@@ -0,0 +1,57 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+/*
+Author: Jonathan Smyth
+Date : 29.04.2021
+Descr: Sorting the array in ascending order, using bucket sort.
+*/
+ 
+
+#define EMPTY 0
+
+
+void bin_sort(int* number, int count) {
+    
+    int temp;
+   // Loop to get the elements stored in array
+ 
+   // Logic of selection sort algorithm
+   for(int i=0;i<count;i++){
+      for(int j=i+1;j<count;j++){
+         if(number[i]>=number[j]){
+            temp=number[i];
+            number[i]=number[j];
+            number[j]=temp;
+         }
+      }
+   }
+
+//    printf("Sorted elements: ");
+//    for(int i=0;i<count;i++)
+//       printf(" %d",number[i]);
+
+// //    return 0;
+    }
+
+
+ 
+/* Driver program to test above function */
+int main()
+{
+    int bin[30] = {0};
+    // int foo [30] = { 2, 2, 1, 0, 1, 0, 1, 1, 0, 2, 2, 1, 1, 1, 2, 0, 0, 1, 1, 0,
+    //                 1, 0, 1, 2, 0, 0, 0, 1, 1, 2 };
+    int foo [30] = { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 2, 0, 0, 1, 1, 0,
+                    0, 1, 1, 2, 2, 2, 0, 1, 1, 2 };
+    int count = sizeof(foo) / sizeof(foo[0]);
+
+    bin_sort(foo,count);
+ 
+    cout << "Sorted array is \n";
+    for (int i = 0; i < count; i++)
+        cout << foo[i] << " ";
+    return 0;
+}
